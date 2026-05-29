f088588a9a38c771381f8e588b01c39959fa127b
OscarMolinaSirvent <omolsir@upv.edu.es>
2026-05-29 11:20:28 +0200

    Proyecto terminado

--- ProyectosII/Proyecto_Final/Funciones.py
+++ ProyectosII/Proyecto_Final/Funciones.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import threading
 from ComunicacionDB import *
 
-robot_ip = "127.0.0.1" 
+robot_ip = "172.16.190.25" 
 
 # Único lock para asegurar que no se solapen comandos en el socket TCP del robot
 lock = threading.Lock()
@@ -124,11 +124,11 @@ def PickPlace():
     registrar_estado("Abierta")
     
     verificar_parada()
-    robot.move(JointsPosition(-0.68, -0.38, -0.30,-0.21,-1.09, -0.60)) 
+    robot.move(JointsPosition(-0.6658,-0.4262,-0.2053,-0.2223,-1.2625,-0.6334)) 
     registrar_estado("Abierta")
     
     verificar_parada()
-    robot.move(JointsPosition(-0.69, -0.47, -0.32, -0.23 , -1.10, -0.58)) 
+    robot.move(JointsPosition(-0.696, -0.474, -0.315, -0.242 , -1.121, -0.604)) 
     registrar_estado("Abierta")
     
     verificar_parada()
@@ -169,12 +169,11 @@ def PickPlace():
     insertar_log(conexion_db, "Cinta Activa", "Cinta")
     registrar_estado("Abierta")
     
-    """while robot.digital_read(sensor_pin_id1) == PinState.HIGH:
+    while robot.digital_read(sensor_pin_id1) == PinState.HIGH:
         verificar_parada()
         modificar_sensor(conexion_db, id_componente=3, nuevo_tipo="1", nuevo_estado= "HIGH" if robot.digital_read(sensor_pin_id1) == PinState.HIGH else "LOW")
         modificar_sensor(conexion_db, id_componente=4, nuevo_tipo="2", nuevo_estado= "HIGH" if robot.digital_read(sensor_pin_id2) == PinState.HIGH else "LOW")
-
-        robot.wait(0.1)""" #Descomentar para activar función de sensor
+        robot.wait(0.1)
 
     verificar_parada()
     robot.wait(0.5)
@@ -185,11 +184,11 @@ def PickPlace():
     registrar_estado("Abierta")
     
     verificar_parada()
-    """if robot.digital_read(sensor_pin_id2) == PinState.LOW:
+    if robot.digital_read(sensor_pin_id2) == PinState.LOW:
         modificar_sensor(conexion_db, id_componente=3, nuevo_tipo="1", nuevo_estado= "HIGH" if robot.digital_read(sensor_pin_id1) == PinState.HIGH else "LOW")
         modificar_sensor(conexion_db, id_componente=4, nuevo_tipo="2", nuevo_estado= "HIGH" if robot.digital_read(sensor_pin_id2) == PinState.HIGH else "LOW")
         Defectuosas()
-        return 1""" #Descomentar para activar función de defectuosas
+        return 1 #Descomentar para activar función de defectuosas"""
         
     verificar_parada()
     robot.run_conveyor(conveyor_id, speed=80, direction=ConveyorDirection.FORWARD)
@@ -211,11 +210,9 @@ def PickPlace():
 
     return 0
 
-def MovimientoMesa1(x,y):
+def MovimientoMesa1():
     verificar_parada()
     robot.release_with_tool()
-    modificar_pinza(conexion_db, id_componente=2, nuevo_estado="Abierta")
-    insertar_log(conexion_db, "Pinza Abierta", "Herramienta")
     registrar_estado("Abierta")
     verificar_parada()
     robot.move(JointsPosition(0.84,-0.99,0.60,-0.02,-1.26,0.02))
@@ -225,30 +222,123 @@ def MovimientoMesa1(x,y):
     registrar_estado("Abierta")
     verificar_parada()
     robot.grasp_with_tool() 
-    modificar_pinza(conexion_db, id_componente=2, nuevo_estado="Cerrada")
-    insertar_log(conexion_db, "Pinza Cerrada", "Herramienta")
     registrar_estado("Cerrada (Objeto)")
     verificar_parada()
+    robot.move(JointsPosition(0.8028,-0.911,0.5688,0.0752,-1.2794,-0.1011))
     robot.move(JointsPosition(-0.05, 0.24, -0.61, -0.01, -0.32, 0.0)) 
     registrar_estado("Cerrada (Objeto)")
     verificar_parada()
-    robot.move(JointsPosition(x,y,-0.99,0.10,-0.70,0.12))
+    robot.move(JointsPosition(1.80,0.03,-0.99,0.10,-0.70,0.12))
     registrar_estado("Cerrada (Objeto)")
     verificar_parada()
-    robot.move(JointsPosition(x,y,-1.03,0.09,-0.50,0.11))
+    robot.move(JointsPosition(1.80,-0.08,-1.03,0.09,-0.50,0.11))
     registrar_estado("Cerrada (Objeto)")
     verificar_parada()
     robot.release_with_tool()
-    modificar_pinza(conexion_db, id_componente=2, nuevo_estado="Abierta")
-    insertar_log(conexion_db, "Pinza Abierta", "Herramienta")
     registrar_estado("Abierta")
     verificar_parada()
-    robot.move(JointsPosition(x,y,-0.99,0.10,-0.70,0.12))
+    robot.move(JointsPosition(1.80,0.03,-0.99,0.10,-0.70,0.12))
     registrar_estado("Abierta")
     verificar_parada()
     robot.move(JointsPosition(-0.05, 0.24, -0.61, -0.01, -0.32, 0.0)) 
     registrar_estado("Abierta")
-    
+
+def MovimientoMesa2():
+    verificar_parada()
+    robot.release_with_tool()
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.84,-0.99,0.60,-0.02,-1.26,0.02))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.84,-0.99,0.52,-0.03,-1.14,0.03))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.grasp_with_tool() 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(0.8028,-0.911,0.5688,0.0752,-1.2794,-0.1011))
+    robot.move(JointsPosition(1.6,0.2,-0.4,-0.14,-1.3,-0.11)) 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(1.6,-0.47,-0.37,0.009,-0.86,-0.08))
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(1.6,-0.47,-0.43,0.02,-0.74,-0.04))
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.release_with_tool()
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(1.6,-0.47,-0.37,0.009,-0.86,-0.08))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(-0.05, 0.24, -0.61, -0.01, -0.32, 0.0)) 
+    registrar_estado("Abierta")
+
+def MovimientoMesa3():
+    verificar_parada()
+    robot.release_with_tool()
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.84,-0.99,0.60,-0.02,-1.26,0.02))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.84,-0.99,0.52,-0.03,-1.14,0.03))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.grasp_with_tool() 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(0.8028,-0.911,0.5688,0.0752,-1.2794,-0.1011))
+    robot.move(JointsPosition(1.6,0.2,-0.4,-0.14,-1.3,-0.11)) 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(0.98,-0.08,-0.86,-0.04,-0.75,-0.5))#Ataque 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(0.98,-0.17,-0.88,-0.07,-0.65,-0.59))#Dejada
+    verificar_parada()
+    robot.release_with_tool()
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.98,-0.08,-0.86,-0.04,-0.75,-0.5))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(-0.05, 0.24, -0.61, -0.01, -0.32, 0.0)) 
+    registrar_estado("Abierta")
+
+def MovimientoMesa4():
+    verificar_parada()
+    robot.release_with_tool()
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.84,-0.99,0.60,-0.02,-1.26,0.02))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(0.84,-0.99,0.52,-0.03,-1.14,0.03))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.grasp_with_tool() 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(0.8028,-0.911,0.5688,0.0752,-1.2794,-0.1011))
+    robot.move(JointsPosition(1.6,0.2,-0.4,-0.14,-1.3,-0.11)) 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(1.2,-0.5,-0.2,0.013,-0.96,-0.44))#Ataque 
+    registrar_estado("Cerrada (Objeto)")
+    verificar_parada()
+    robot.move(JointsPosition(1.2,-0.62,-0.2,-0.038,-0.9,-0.38))#Dejada
+    verificar_parada()
+    robot.release_with_tool()
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(1.2,-0.5,-0.2,0.013,-0.96,-0.44))
+    registrar_estado("Abierta")
+    verificar_parada()
+    robot.move(JointsPosition(-0.05, 0.24, -0.61, -0.01, -0.32, 0.0)) 
+    registrar_estado("Abierta")
 
     
 def Defectuosas():
@@ -275,25 +365,34 @@ def detener_ciclo_automatico():
     """Activa el flag de parada urgente para romper la ejecución del bucle."""
     stop_urgente.set()
 
-x =1.8
-y = 0.03
+x =-36
+y = 155
 def automatico():
     stop_urgente.clear()
     global x, y
+    contador = 0
     actualizar_estado_robot(conexion_db, id_robot=1, nuevo_estado="En Ejecución")
     with lock:
         try:
-            for i in range(1,5):
-                PickPlace()
-                MovimientoMesa1(x,y)
-                modificar_ocupado(conexion_db, id_tablero=1, identificador_casilla=i, nuevo_estado="Ocupado")
-                if i == 0:
-                    y += 0.05
-                elif i == 1:
-                    x += 0.05
-                    y = 0.23
-                elif i == 2:
-                    y += 0.05
+            while True:
+                if contador == 4:
+                        contador = 0
+                        break
+                defect = PickPlace()
+                modificar_ocupado(conexion_db, id_tablero=1, identificador_casilla=contador, nuevo_estado="Ocupado")
+                if defect != 1:
+                    
+                    if contador == 0:
+                        MovimientoMesa1()
+                    elif contador == 1:
+                        MovimientoMesa2()
+                    elif contador == 2:
+                        MovimientoMesa3()
+                    elif contador == 3:
+                        MovimientoMesa4()
+                    contador += 1
+                print(contador)
+
             actualizar_estado_robot(conexion_db, id_robot=1, nuevo_estado="En Espera")
             for i in range(1,5):
                 modificar_ocupado(conexion_db, id_tablero=1, identificador_casilla=i, nuevo_estado="Libre")
